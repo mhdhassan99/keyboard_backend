fe30bf3287e3717290ae974fad487af0bf197b8b
mhdhassan99 <mhassan.work99@gmail.com>
2020-08-14 17:38:29 -0400

    data is persisting

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,18 @@ class ItemsController < ApplicationController
         @items = Item.all 
         render json: @items
     end
+
+     def update 
+        # byebug
+        @item = Item.find(params[:id])
+        
+        @item.update(item_params)
+        # byebug
+        render json: @item
+        
+    end 
+
+    def item_params
+        params.require(:item).permit(:name, :image, :category, :price, :user_id)
+    end 
 end
